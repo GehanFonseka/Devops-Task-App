9751a764143b275c6b6ee0da0a8047c7eca39ea8
Gehan Fonseka <gehanfonseka@gmail.com>
2025-10-31 22:06:42 +0530

    update TaskList

--- task-frontend/src/TaskList.js
+++ task-frontend/src/TaskList.js
@@ -28,7 +28,7 @@ const TaskList = () => {
     if (loading) {
         return (
             <div className="task-list-container">
-                <p className="status-message status-loading">LoadingGG tasks...</p>
+                <p className="status-message status-loading">LoadingG tasks...</p>
             </div>
         );
     }
@@ -44,7 +44,7 @@ const TaskList = () => {
     return (
         // Apply the main container class here
         <div className="task-list-container">
-            <h1>My Tasks</h1>
+            <h1>My Tasksss</h1>
             {/* Apply the list class here */}
             <ul className="task-list">
                 {tasks.map(task => (
