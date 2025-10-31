59b16805d7fff3403c937c88f4deae1c30df105e
Gehan Fonseka <gehanfonseka@gmail.com>
2025-11-01 03:57:57 +0530

    update deploy.yml

--- task_api/task_api/src/main/java/com/example/task_api/TaskController.java
+++ task_api/task_api/src/main/java/com/example/task_api/TaskController.java
@@ -1,10 +1,10 @@
 package com.example.task_api;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api") // All endpoints in this class will start with /api
