689673cb6c4fe47f9e8d83880512e524a54425f8
Gehan Fonseka <gehanfonseka@gmail.com>
2025-12-15 20:05:54 +0530

    Add Ingress and ManagedCertificate

--- task_api/task_api/src/main/java/com/example/task_api/TaskController.java
+++ task_api/task_api/src/main/java/com/example/task_api/TaskController.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.annotation.PostConstruct;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class TaskController {
